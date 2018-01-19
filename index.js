fb5cce8104df076adbaab37de3f54a8d3bad36a4
seabelle mcfarlane <seabellemcfarlane@aol.com>
2018-01-19 21:36:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+ function kittens() {
+  return kittens
+ }
+ var kittens = ["Milo", "Otis", "Garfield"]
+ function destructivelyAppendKitten(wiskers) {
+   kittens.push(wiskers)
+ }
+ var kittens = ["Milo", "Otis", "Garfield"]
+ function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+ }
+ var kittens = ["Milo", "Otis", "Garfield"]
+ function destructivelyRemoveLastKitten() {
+  kittens.pop()
+ }
+ var kittens = ["Milo", "Otis", "Garfield"]
+ function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+ }
+ var kittens = ["Milo", "Otis", "Garfield"]
+ function appendKitten(name) {
+   var newArr = kittens.slice()
+   newArr.push(name)
+   return newArr
+ }
+
+ var kittens = ["Milo", "Otis", "Garfield"]
+ function prependKitten(name) {
+  var newArr = kittens.slice()
+  newArr.unshift(name)
+  return newArr
+ }
+
+ var kittens = ["Milo", "Otis", "Garfield"]
+ function removeLastKitten(name) {
+   var newArr = kittens.slice()
+  newArr.pop()
+ return newArr
+ }
+ var kittens = ["Milo", "Otis", "Garfield"]
+ function removeFirstKitten(name) {
+   var newArr = kittens.slice()
+  newArr.shift()
+   return newArr
+ }
